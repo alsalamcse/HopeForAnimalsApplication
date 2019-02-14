98a36abf9ad9b5fdf6f809852ed220b2fa98eba2
JamalSamniya <jamal.samniya@gmail.com>
2019-02-14 14:49:33 +0200

    hopeForAnimals

---
+++ app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.samniya.jamal.hopeforanimalsapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        MyThread myThread=new MyThread();
+        myThread.start();
+        super.onResume();
+    }
+
+    public  class MyThread extends Thread
+    {
+        @Override
+        public void run() {
+
+            try {
+                sleep(3000);
+                Intent i=new Intent(getApplicationContext(),null);
+                startActivity(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
