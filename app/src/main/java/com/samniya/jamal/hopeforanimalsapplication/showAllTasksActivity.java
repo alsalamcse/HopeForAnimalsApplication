3ff028d69d529dd4815aa71b13da35d871253e6a
JamalSamniya <jamal.samniya@gmail.com>
2019-02-24 11:34:26 +0200

    hopeForAnimals

---
+++ app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/showAllTasksActivity.java
@@ -0,0 +1,18 @@
+package com.samniya.jamal.hopeforanimalsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class showAllTasksActivity extends AppCompatActivity {
+
+    private ListView LvTask;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_tasks);
+
+    }
+}
