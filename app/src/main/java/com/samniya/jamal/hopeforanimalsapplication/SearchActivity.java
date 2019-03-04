a8c6da35eb5c8642c9f855de1154063a2a93dafc
JamalSamniya <jamal.samniya@gmail.com>
2019-03-04 14:04:59 +0200

    hopeForAnimals

--- app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/SearchActivity.java
+++ app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/SearchActivity.java
@@ -5,16 +5,56 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Button search;
-
+    private Spinner AgeSpinner , KindSpinner ;
+    private  String [] InfoList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Button search = (Button) findViewById(R.id.searchBtn);
+
+        Spinner KindSpinner = (Spinner) findViewById(R.id.spinner2ID);
+        Spinner AgeSpinner = (Spinner) findViewById(R.id.spinnerId);
+        // Spinner click listener
+        AgeSpinner.setOnItemSelectedListener(this);
+        KindSpinner.setOnItemSelectedListener(this);
+
+        List<String> Kind = new ArrayList<String>();
+        Kind.add("Dog");
+        Kind.add("Cat");
+        Kind.add("Bird");
+        Kind.add("Rabbit");
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("0-2");
+        categories.add("2-5");
+        categories.add("5-10");
+        categories.add("10+");
+
+        // Creating adapter for AgeSpinner
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, Kind);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to AgeSpinner and kindSpinner
+        AgeSpinner.setAdapter(dataAdapter);
+        KindSpinner.setAdapter(dataAdapter2);
+
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +65,25 @@ public class SearchActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        //String item = parent.getItemAtPosition(position).toString();
+       // InfoList[0] = item;
+        switch (parent.getId()){
+            case R.id.spinnerId :
+                String item = parent.getItemAtPosition(position).toString();
+                InfoList[1] = item;
+                break;
+            case R.id.spinner2ID :
+                String item2 = parent.getItemAtPosition(position).toString();
+                InfoList[0] = item2;
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
