80cde43b9d0c1d9cb4b8261209800e752ea54c9f
Owner <noran@danon.tzafonet.org.il>
2019-06-09 13:23:48 +0300

    لغغبلغعل

--- app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/MyAnimalAdapter.java
+++ app/src/main/java/com/samniya/jamal/hopeforanimalsapplication/MyAnimalAdapter.java
@@ -1,48 +1,51 @@
 package com.samniya.jamal.hopeforanimalsapplication;
 
-public class MyAnimalAdapter {
 
-    public class MyAnimalAdapter extends ArrayAdapter<Animal> {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MyAnimalAdapter extends ArrayAdapter<MyAnimal> {
 
 
+    public MyAnimalAdapter(Context context, int resource) {
 
-        public MypatientAdapter(Context context, int resource) {
+        super(context, resource);
 
-            super(context, resource);
+    }
+
+
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.animal_item, parent, false);
+
+
+            final MyAnimal mypatient = getItem(position); // return data object number"pos"
+//
+//            TextView id = convertView.findViewById(R.id.id);
+//
+//
+//            ImageButton edit = convertView.findViewById(R.id.edit);
+//
+//            ImageButton medicines = convertView.findViewById(R.id.medicines);
+//
+//
+//            id.setText(mypatient.getId());
+//
 
         }
 
 
 
-        @Override
+    }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-
-
-
-
-            if (convertView == null) {
-
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.patient_item, parent, false);
-
-
-
-
-
-                final Mypatient mypatient = getItem(position); // return data object number"pos"
-
-                TextView id = convertView.findViewById(R.id.id);
-
-
-
-                ImageButton edit = convertView.findViewById(R.id.edit);
-
-                ImageButton medicines = convertView.findViewById(R.id.medicines);
-
-
-
-                id.setText(mypatient.getId());
-
-
-            }
+}
